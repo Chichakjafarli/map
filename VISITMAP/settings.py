ed13f0d73be785b563056a5e9710e4c15d3247a1
Chichakjafarli <chichakjafarli@gmail.com>
2025-01-30 22:06:33 +0400

    Configure env variables

--- VISITMAP/settings.py
+++ VISITMAP/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +30,7 @@ SECRET_KEY = 'django-insecure-sukbqz0y!@zhi=l1s)s49gc_2%cx_yc!4r#r3^@ac0vt^+uagu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
@@ -131,5 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'banujafarli9@gmail.com'  # Əsas mail ünvanınız
-EMAIL_HOST_PASSWORD = 'srun oejt yysj qzgy'  # Mailin şifrəsi
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
